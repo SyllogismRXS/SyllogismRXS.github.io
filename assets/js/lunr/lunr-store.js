467c30633b88f35bac0773e5f8e06c793fc4aef1
Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
2020-03-31 00:45:43 -0400

    static: use https in url

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Remote Pair Programming via SSH and wemux on Linux",
         "excerpt":"There are several benefits to the use of pair programming in the software development process: fewer mistakes, better understanding of the codebase across multiple developers, and improved team cohesiveness (cf. The Pros and Cons of Pair Programming). Personally, I have found pair programming can be especially useful for training. Experienced...","categories": ["ssh","programming","ngrok","tmux","wemux","terminal"],
         "tags": [],
-        "url": "http://kevindemarco.com/ssh/programming/ngrok/tmux/wemux/terminal/2020/03/25/pair-programming.html",
+        "url": "https://kevindemarco.com/ssh/programming/ngrok/tmux/wemux/terminal/2020/03/25/pair-programming.html",
         "teaser": null
       }]
