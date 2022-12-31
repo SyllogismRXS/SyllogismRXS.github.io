9fce637d14771624e38001ec03be3ca27103d63a
Kevin DeMarco <kevin@kevindemarco.com>
2022-12-31 16:15:22 -0500

    Added ROS Docker

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,24 +2,30 @@ var store = [{
         "title": "Remote Pair Programming via SSH and wemux on Linux",
         "excerpt":"There are several benefits to the use of pair programming in the software development process: fewer mistakes, better understanding of the codebase across multiple developers, and improved team cohesiveness (cf. The Pros and Cons of Pair Programming). Personally, I have found pair programming can be especially useful for training. Experienced...","categories": ["ssh","programming","ngrok","tmux","wemux","terminal"],
         "tags": [],
-        "url": "https://kevindemarco.com/ssh/programming/ngrok/tmux/wemux/terminal/2020/03/25/pair-programming.html",
-        "teaser": "https://kevindemarco.com/assets/images/2020-03-25-pair-programming/teaser.png"
+        "url": "/ssh/programming/ngrok/tmux/wemux/terminal/2020/03/25/pair-programming.html",
+        "teaser": "/assets/images/2020-03-25-pair-programming/teaser.png"
       },{
         "title": "A Handcrafted Dovetail Box",
         "excerpt":"My educational background is in Electrical and Computer Engineering with a strong bent towards Computer Science. This means that I spend most of my day typing on a computer, reading technical papers, designing systems, writing code, and communicating with team members. Even when I am designing mechanical systems for the...","categories": ["woodworking","handtools","box"],
         "tags": [],
-        "url": "https://kevindemarco.com/woodworking/handtools/box/2021/01/15/dovetail-box.html",
-        "teaser": "https://kevindemarco.com/assets/images/2021-01-15-dovetail-box/teaser.jpg"
+        "url": "/woodworking/handtools/box/2021/01/15/dovetail-box.html",
+        "teaser": "/assets/images/2021-01-15-dovetail-box/teaser.jpg"
       },{
         "title": "Bamboomba: The sort of wooden robot.",
         "excerpt":"What do you get when you cross a Roomba… with scrap wood? You get… Bamboomba! You might be wondering why Kevin, being the professional researcher who he is, decided to build a half-wooden monstrosity with an obsolete robot base. Didn’t Kevin go to college for thirteen years or something? Why...","categories": ["robotics","ROS","woodworking"],
         "tags": [],
-        "url": "https://kevindemarco.com/robotics/ros/woodworking/2021/01/19/bamboomba-part-1.html",
-        "teaser": "https://kevindemarco.com/assets/images/2021-01-19-bamboomba-part-1/teaser.jpg"
+        "url": "/robotics/ros/woodworking/2021/01/19/bamboomba-part-1.html",
+        "teaser": "/assets/images/2021-01-19-bamboomba-part-1/teaser.jpg"
       },{
         "title": "A hoarder's guide to wood storage",
         "excerpt":"Do you actually have a hoarding problem if you can build efficient storage solutions for things that you can’t throw away? I’m not going to answer that question because I probably won’t like the answer. It’s not that I have a hoarding problem. It’s that I don’t like waste. I...","categories": ["woodworking","storage"],
         "tags": [],
-        "url": "https://kevindemarco.com/woodworking/storage/2021/02/22/wood-rack.html",
-        "teaser": "https://kevindemarco.com/assets/images/2021-02-22-wood-rack/wood-on-rack-2.jpg"
+        "url": "/woodworking/storage/2021/02/22/wood-rack.html",
+        "teaser": "/assets/images/2021-02-22-wood-rack/wood-on-rack-2.jpg"
+      },{
+        "title": "ROS Development in Docker",
+        "excerpt":"If you use the Robot Operating System (ROS) and you are frusterated with system dependencies, incompatible ROS / Linux operating system versions, and non-determinstic builds, then this article is for you. For each of my ROS development workspaces, I create a Docker development environment to isolate the ROS environment from...","categories": ["ROS","Docker","docker-compose","robotics","programming","development"],
+        "tags": [],
+        "url": "/ros/docker/docker-compose/robotics/programming/development/2022/12/28/ros-docker.html",
+        "teaser": "/assets/images/2022-12-28-ros-docker/docker-ros.png"
       }]
